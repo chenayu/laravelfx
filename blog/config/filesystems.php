f0e0d62763f21d80adba24dc8a66b9e0f67a61c1
s <xiangyu@czxy.com>
2018-08-30 18:27:29 +0800

    更换数据库

--- blog/config/filesystems.php
+++ blog/config/filesystems.php
@@ -55,7 +55,7 @@ return [
             'visibility' => 'public',
         ],
 
-        'public' => [
+        'uploads' => [
             'driver' => 'local',
             'root' => public_path('uploads'),
             'url' => env('APP_URL').'/uploads',
